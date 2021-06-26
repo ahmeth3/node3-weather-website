7d264734d03776aaa942b4c60231100c8bed1547
Ahmet Halilovic <akihalilovic@gmail.com>
2021-06-26 14:33:01 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup habdlebars(hbs) engine and views location 
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to server
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    }) // preko drugog parametra saljemo podatke dinamickom htmlu
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    }) // preko drugog parametra saljemo podatke dinamickom htmlu
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Andrew Mead'
+    }) // preko drugog parametra saljemo podatke dinamickom htmlu
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Address must be provided'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({error}) // return da bi se break
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                returnres.send({error})
+            }
+
+            res.send({
+                location: location,
+                forecast: forecastData,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    res.send({
+        products: []
+    })
+})
+
+// matchuje sve sto pocinje sa /help
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found'
+    })
+})
+
+// * match anything that isnt listed
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
